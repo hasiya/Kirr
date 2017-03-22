fa2e92271d7a638bbdfd35b19b9ca7f83d8a7e8f
hasiya <hasith002@yahoo.com>
2017-03-22 20:44:12 +0000

    All Files

--- Kirr/old_settings.py
+++ Kirr/old_settings.py
@@ -61,8 +61,8 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'Kirr.urls'
 ROOT_HOSTCONF = 'Kirr.hosts'
 DEFAULT_HOST = 'www'
-DEFAULT_REDIRECT_URL = "http://www.tirr.com:8000"
-PARENT_HOST = "tirr.com"
+DEFAULT_REDIRECT_URL = "http://www.tirr.com:5000"
+PARENT_HOST = "tirr.com:5000"
 
 TEMPLATES = [
     {
